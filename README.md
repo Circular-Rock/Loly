4e6ad542d8b0ae7fd83ee4e6ac661bf3f565972e
gwh12138 <gongwh12138@gmail.com>
2025-03-14 17:52:16 +0800

    辅助文件补充

---
+++ README.md
@@ -0,0 +1,15 @@
+[toc]
+
+# 项目简介
+
++ 项目名称: 虚拟数字主播
++ 项目描述: 本项目包含基于vue开发的用于`虚拟数字主播`使用的前端页面和基于falsk的后端登录注册功能以及数据库通信
++ 团队名: 循环摇滚
++ 开发者: 循环团队-宫文浩
+
+# 项目需求
+
++ 项目首先需要安装NodeJS、npm、falsk
++ 之后使用src\db_utils中的db_loly_user_login.sql 创建数据库和表
++ 然后使用src\requirements.txt 添加对应的依赖库
++ 最后启动app.py和App.vue
--- src/db_utils/db_loly_user_login.sql
+++ src/db_utils/db_loly_user_login.sql
@@ -1,3 +1,17 @@
+-- 创建数据库
+CREATE DATABASE IF NOT EXISTS db_loly;
+
+-- 使用数据库
+USE db_loly;
+
+-- 创建表
+CREATE TABLE IF NOT EXISTS user_login (
+    id INT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
+
+-- 插入数据
 INSERT INTO db_loly.user_login (id, user_name, user_password) VALUES (32, 'user123', '12345678');
 INSERT INTO db_loly.user_login (id, user_name, user_password) VALUES (33, 'user12345', '12345678');
 INSERT INTO db_loly.user_login (id, user_name, user_password) VALUES (34, 'hyj', '12345678');
